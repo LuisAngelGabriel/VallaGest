5c897d64948f01c14b30f676b37cfdff7190ffc7
LuisAngelGabriel <luis_gabriel1@ucne.edu.do>
2026-03-30 17:11:56 -0500

    Crea la interface del repositorio para la autenticacion

---
+++ app/src/main/java/edu/ucne/vallagest/domain/usuarios/repository/AuthRepository.kt
@@ -0,0 +1,11 @@
+package edu.ucne.vallagest.domain.usuarios.repository
+
+import edu.ucne.vallagest.data.remote.Resource
+import edu.ucne.vallagest.domain.usuarios.model.Usuario
+import kotlinx.coroutines.flow.Flow
+
+interface AuthRepository {
+    fun login(email: String, password: String): Flow<Resource<Usuario>>
+    fun registro(nombre: String, email: String, password: String): Flow<Resource<Usuario>>
+    fun getSession(): Flow<Usuario?>
+}
