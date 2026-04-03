096bdc77bdc36389ac1fcb57ddcd3e415d54f812
LuisAngelGabriel <luis_gabriel1@ucne.edu.do>
2026-04-02 23:48:17 -0500

    Añade el Register a la clase Screen

--- app/src/main/java/edu/ucne/vallagest/presentation/navigation/Screen.kt
+++ app/src/main/java/edu/ucne/vallagest/presentation/navigation/Screen.kt
@@ -2,11 +2,13 @@ package edu.ucne.vallagest.presentation.navigation
 
 import kotlinx.serialization.Serializable
 
-
 sealed class Screen {
     @Serializable
     object Login : Screen()
 
     @Serializable
     object Home : Screen()
+
+    @Serializable
+    object Register : Screen()
 }
