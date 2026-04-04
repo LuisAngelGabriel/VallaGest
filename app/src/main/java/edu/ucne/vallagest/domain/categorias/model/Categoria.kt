b2895b5311a96e9dbdef72e8d044b4fa72ae6184
LuisAngelGabriel <luis_gabriel1@ucne.edu.do>
2026-04-03 22:35:27 -0500

    Agrega el Rol al Modelo de Categoria

--- app/src/main/java/edu/ucne/vallagest/domain/categorias/model/Categoria.kt
+++ app/src/main/java/edu/ucne/vallagest/domain/categorias/model/Categoria.kt
@@ -3,5 +3,6 @@ package edu.ucne.vallagest.domain.categorias.model
 data class Categoria(
     val categoriaId: Int,
     val nombre: String,
-    val descripcion: String
+    val descripcion: String,
+    val rol: String? = null
 )
