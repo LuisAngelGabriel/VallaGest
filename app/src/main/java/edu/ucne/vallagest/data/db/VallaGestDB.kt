e55f49841b277f972714d16b7bd19aaea83c16bf
LuisAngelGabriel <luis_gabriel1@ucne.edu.do>
2026-04-03 22:35:02 -0500

    Agrega Categoria al NavHost

--- app/src/main/java/edu/ucne/vallagest/data/db/VallaGestDB.kt
+++ app/src/main/java/edu/ucne/vallagest/data/db/VallaGestDB.kt
@@ -2,20 +2,24 @@ package edu.ucne.vallagest.data.db
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import edu.ucne.vallagest.data.local.dao.CategoriaDao
 import edu.ucne.vallagest.data.local.dao.UsuarioDao
 import edu.ucne.vallagest.data.local.dao.VallaDao
+import edu.ucne.vallagest.data.local.entities.CategoriaEntity
 import edu.ucne.vallagest.data.local.entities.UsuarioEntity
 import edu.ucne.vallagest.data.local.entities.VallaEntity
 
 @Database(
     entities = [
         VallaEntity::class,
-        UsuarioEntity::class
+        UsuarioEntity::class,
+        CategoriaEntity::class
     ],
-    version = 2,
+    version = 3,
     exportSchema = false
 )
 abstract class VallaGestDb : RoomDatabase() {
     abstract fun vallaDao(): VallaDao
     abstract fun usuarioDao(): UsuarioDao
+    abstract fun categoriaDao(): CategoriaDao
 }
