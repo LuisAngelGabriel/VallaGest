10594593c277ba4472d0cbee50dcb537e9cad3cb
LuisAngelGabriel <luis_gabriel1@ucne.edu.do>
2026-04-06 09:08:50 -0500

    Crea

---
+++ app/src/main/java/edu/ucne/vallagest/data/local/dao/VallaDao.kt
@@ -0,0 +1,32 @@
+package edu.ucne.vallagest.data.local.dao
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Query
+import androidx.room.Upsert
+import edu.ucne.vallagest.data.local.entities.VallaEntity
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface VallaDao {
+    @Query("SELECT * FROM Vallas ORDER BY vallaId DESC")
+    fun observerAll(): Flow<List<VallaEntity>>
+
+    @Query("SELECT * FROM Vallas WHERE vallaId = :id")
+    suspend fun getById(id: Int): VallaEntity?
+
+    @Upsert
+    suspend fun upsert(valla: VallaEntity): Long
+
+    @Delete
+    suspend fun delete(valla: VallaEntity)
+
+    @Query("DELETE FROM Vallas WHERE vallaId = :id")
+    suspend fun deleteById(id: Int)
+
+    @Query("DELETE FROM Vallas")
+    suspend fun clearAll()
+
+    @Query("SELECT * FROM Vallas WHERE nombre = :nombre")
+    suspend fun getVallasByNombre(nombre: String): List<VallaEntity>
+}
