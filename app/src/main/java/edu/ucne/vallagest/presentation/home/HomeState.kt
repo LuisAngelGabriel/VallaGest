f57fe0f000f3e8f918672e9c306d7245950b3e98
LuisAngelGabriel <luis_gabriel1@ucne.edu.do>
2026-04-06 23:17:04 -0500

    Agrega el cnteo del carrito al Estado de Home

--- app/src/main/java/edu/ucne/vallagest/presentation/home/HomeState.kt
+++ app/src/main/java/edu/ucne/vallagest/presentation/home/HomeState.kt
@@ -5,5 +5,6 @@ import edu.ucne.vallagest.domain.vallas.model.Valla
 data class HomeState(
     val isLoading: Boolean = false,
     val vallas: List<Valla> = emptyList(),
-    val error: String = ""
+    val error: String = "",
+    val carritoCount: Int = 0
 )
