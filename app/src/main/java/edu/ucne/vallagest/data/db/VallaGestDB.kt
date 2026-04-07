d457f5cae389419ae2b0852d7e3f92ca87fba88b
LuisAngelGabriel <luis_gabriel1@ucne.edu.do>
2026-04-06 23:15:28 -0500

    Agrega el CarritoEntity y el Dao a la DataBase

--- app/src/main/java/edu/ucne/vallagest/data/db/VallaGestDB.kt
+++ app/src/main/java/edu/ucne/vallagest/data/db/VallaGestDB.kt
@@ -2,24 +2,30 @@ package edu.ucne.vallagest.data.db
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import edu.ucne.vallagest.data.local.dao.CarritoDao
 import edu.ucne.vallagest.data.local.dao.CategoriaDao
 import edu.ucne.vallagest.data.local.dao.UsuarioDao
 import edu.ucne.vallagest.data.local.dao.VallaDao
 import edu.ucne.vallagest.data.local.entities.CategoriaEntity
 import edu.ucne.vallagest.data.local.entities.UsuarioEntity
 import edu.ucne.vallagest.data.local.entities.VallaEntity
+import edu.ucne.vallagest.data.local.entities.CarritoEntity
 
 @Database(
     entities = [
         VallaEntity::class,
         UsuarioEntity::class,
-        CategoriaEntity::class
+        CategoriaEntity::class,
+        CarritoEntity::class
+
     ],
-    version = 3,
+    version = 4,
     exportSchema = false
 )
 abstract class VallaGestDb : RoomDatabase() {
     abstract fun vallaDao(): VallaDao
     abstract fun usuarioDao(): UsuarioDao
     abstract fun categoriaDao(): CategoriaDao
+
+    abstract fun carritoDao(): CarritoDao
 }
