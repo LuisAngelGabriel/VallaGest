9a8d468ebcb4e69c9eaac1038740a22019b14de2
LuisAngelGabriel <luis_gabriel1@ucne.edu.do>
2026-04-07 23:14:20 -0500

    Crea OrdenDto

---
+++ app/src/main/java/edu/ucne/vallagest/data/local/entities/OrdenEntity.kt
@@ -0,0 +1,25 @@
+package edu.ucne.vallagest.data.local.entities
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "Ordenes")
+data class OrdenEntity(
+    @PrimaryKey val ordenId: Int,
+    val usuarioId: Int,
+    val fechaOrden: String,
+    val total: Double,
+    val metodo: String,
+    val estado: String,
+    val comprobanteUrl: String?
+)
+
+@Entity(tableName = "OrdenesDetalles")
+data class OrdenDetalleEntity(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
+    val ordenId: Int,
+    val vallaId: Int,
+    val nombreValla: String,
+    val precioAplicado: Double,
+    val meses: Int
+)
