1b277afd7b1f8c8fd72d6c1a2f329bf53dbd6157
LuisAngelGabriel <luis_gabriel1@ucne.edu.do>
2026-04-07 23:54:32 -0500

    Agrega OrdenEntity a la DB

--- app/src/main/java/edu/ucne/vallagest/data/db/VallaGestDB.kt
+++ app/src/main/java/edu/ucne/vallagest/data/db/VallaGestDB.kt
@@ -6,26 +6,31 @@ import edu.ucne.vallagest.data.local.dao.CarritoDao
 import edu.ucne.vallagest.data.local.dao.CategoriaDao
 import edu.ucne.vallagest.data.local.dao.UsuarioDao
 import edu.ucne.vallagest.data.local.dao.VallaDao
+import edu.ucne.vallagest.data.local.dao.OrdenDao
 import edu.ucne.vallagest.data.local.entities.CategoriaEntity
 import edu.ucne.vallagest.data.local.entities.UsuarioEntity
 import edu.ucne.vallagest.data.local.entities.VallaEntity
 import edu.ucne.vallagest.data.local.entities.CarritoEntity
+import edu.ucne.vallagest.data.local.entities.OrdenEntity
+import edu.ucne.vallagest.data.local.entities.OrdenDetalleEntity
 
 @Database(
     entities = [
         VallaEntity::class,
         UsuarioEntity::class,
         CategoriaEntity::class,
-        CarritoEntity::class
-
+        CarritoEntity::class,
+        OrdenEntity::class,
+        OrdenDetalleEntity::class
     ],
-    version = 4,
+    version = 5,
     exportSchema = false
 )
 abstract class VallaGestDb : RoomDatabase() {
     abstract fun vallaDao(): VallaDao
     abstract fun usuarioDao(): UsuarioDao
     abstract fun categoriaDao(): CategoriaDao
-
     abstract fun carritoDao(): CarritoDao
+
+    abstract fun ordenDao(): OrdenDao
 }
