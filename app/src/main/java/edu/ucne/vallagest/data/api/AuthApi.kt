b8cc6204f4b7f5b53ea94299c2224b040feee394
LuisAngelGabriel <luis_gabriel1@ucne.edu.do>
2026-04-08 00:35:16 -0500

    Agrega el Endpoint de ActualizarPerfil a la Interface de la Api

--- app/src/main/java/edu/ucne/vallagest/data/api/AuthApi.kt
+++ app/src/main/java/edu/ucne/vallagest/data/api/AuthApi.kt
@@ -2,10 +2,12 @@ package edu.ucne.vallagest.data.api
 
 import edu.ucne.vallagest.data.remote.dto.LoginRequest
 import edu.ucne.vallagest.data.remote.dto.RegisterRequest
+import edu.ucne.vallagest.data.remote.dto.AuthResponse
 import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.POST
-import edu.ucne.vallagest.data.remote.dto.AuthResponse
+import retrofit2.http.PUT
+import retrofit2.http.Path
 
 interface AuthApi {
     @POST("api/Usuarios/Login")
@@ -13,4 +15,10 @@ interface AuthApi {
 
     @POST("api/Usuarios/Registrar")
     suspend fun registro(@Body request: RegisterRequest): Response<AuthResponse>
+
+    @PUT("api/Usuarios/Actualizar/{id}")
+    suspend fun actualizarUsuario(
+        @Path("id") id: Int,
+        @Body request: RegisterRequest
+    ): Response<AuthResponse>
 }
